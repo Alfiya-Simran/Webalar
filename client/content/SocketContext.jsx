ee8bf39da0bd2ed6bd45ec1fec4fd00246e4ea05
Alfiya Simran <alfiyasimran05@gmail.com>
2025-07-03 16:24:48 +0530

    SocketContext.jsx

--- client/content/SocketContext.jsx
+++ client/content/SocketContext.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-import { createContext, useContext, useEffect, useRef } from "react";
+import React,{ createContext, useContext, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext();
