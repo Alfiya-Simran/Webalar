5e1555ab7734acd187977373ffa43f5bef8e8484
Alfiya Simran <alfiyasimran05@gmail.com>
2025-07-03 16:08:46 +0530

    SocketContext.jsx

---
+++ client/content/SocketContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useEffect, useRef } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext();
+
+export const useSocket = () => useContext(SocketContext);
+
+export const SocketProvider = ({ children }) => {
+  const socketRef = useRef();
+
+  useEffect(() => {
+    socketRef.current = io(import.meta.env.VITE_BACKEND_URL);
+    return () => socketRef.current.disconnect();
+  }, []);
+
+  return (
+    <SocketContext.Provider value={socketRef.current}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
