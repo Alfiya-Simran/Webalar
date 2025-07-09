a47a2caf0e79caba23b2b9438ae4e7307741c48c
Alfiya Simran <alfiyasimran05@gmail.com>
2025-07-09 11:59:30 +0530

    Update ActionLog.js

--- server/models/ActionLog.js
+++ server/models/ActionLog.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const logSchema = new mongoose.Schema({
-  taskId: { type: mongoose.Schema.Types.ObjectId, ref: "Task" },
-  action: String,
+const actionLogSchema = new mongoose.Schema({
+  taskId: { type: mongoose.Schema.Types.ObjectId, ref: "Task", required: true },
+  action: { type: String, required: true },
   performedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   timestamp: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("ActionLog", logSchema);
+const ActionLog = mongoose.model("ActionLog", actionLogSchema);
+export default ActionLog;
