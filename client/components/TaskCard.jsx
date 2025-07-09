26c90e949135f3593cf976273342c12858ce6009
Alfiya Simran <alfiyasimran05@gmail.com>
2025-07-09 13:19:18 +0530

    Update TaskCard.jsx

--- client/components/TaskCard.jsx
+++ client/components/TaskCard.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-
 import axios from "axios";
+import { Draggable } from "react-beautiful-dnd";
+import { useSocket } from "../content/SocketContext";
 
 export default function TaskCard({ task, index }) {
   const socket = useSocket();
 
-const handleSmartAssign = async () => {
-  try {
-    const res = await axios.post(
-      `${import.meta.env.VITE_BACKEND_URL}/tasks/${task._id}/smart-assign`,
-      {},
-      { headers: { Authorization: localStorage.getItem("token") } }
-    );
-    alert(`Assigned to ${res.data.assignedTo.name}`);
-    socket.emit("taskUpdated", res.data);
-  } catch (err) {
-    alert("Smart assign failed");
-  }
-};
-
+  const handleSmartAssign = async () => {
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/tasks/${task._id}/smart-assign`,
+        {},
+        { headers: { Authorization: localStorage.getItem("token") } }
+      );
+      alert(`Assigned to ${res.data.assignedTo.name}`);
+      socket.emit("taskUpdated", res.data);
+    } catch (err) {
+      alert("Smart assign failed");
+    }
+  };
 
   return (
     <Draggable draggableId={task._id} index={index}>
