12b3b8b67317342c056954c9d07c1f3c72f4afc0
Alfiya Simran <alfiyasimran05@gmail.com>
2025-07-09 11:25:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
-const express = require("express");
-const http = require("http");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const socketio = require("socket.io");
+import express from "express";
+import http from "http";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+
+import authRoutes from "./routes/auth.js";
+import taskRoutes from "./routes/task.js";
+import logRoutes from "./routes/logs.js";
+import setupSocket from "./sockets/taskSocket.js";
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server, {
+const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"]
@@ -20,13 +25,14 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/tasks", require("./routes/task"));
-app.use("/api/logs", require("./routes/logs"));
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/logs", logRoutes);
 
-// Socket setup
-require("./sockets/taskSocket")(io);
+// Sockets
+setupSocket(io);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
