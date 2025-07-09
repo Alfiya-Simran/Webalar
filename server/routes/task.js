5da4c753b601e617c2ed434af11531ff1cc2e726
Alfiya Simran <alfiyasimran05@gmail.com>
2025-07-09 11:28:32 +0530

    Update task.js

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,17 +1,21 @@
-const express = require("express");
+import express from "express";
+import auth from "../middlewares/authMiddleware.js";
+import {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+  getLogs,
+  smartAssignTask
+} from "../controllers/taskController.js";
+
 const router = express.Router();
-const auth = require("../middlewares/authMiddleware");
-const {
-  getTasks, createTask, updateTask, deleteTask, getLogs
-} = require("../controllers/taskController");
-const { smartAssignTask } = require("../controllers/taskController");
 
 router.post("/:id/smart-assign", auth, smartAssignTask);
-
 router.get("/", auth, getTasks);
 router.post("/", auth, createTask);
 router.put("/:id", auth, updateTask);
 router.delete("/:id", auth, deleteTask);
 router.get("/logs", auth, getLogs);
 
-module.exports = router;
+export default router;
