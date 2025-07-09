82579f0de885bdb8fe07af47ec2cad07fa1ad194
Alfiya Simran <alfiyasimran05@gmail.com>
2025-07-09 12:23:43 +0530

    Task.js

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,11 +1,16 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
+  status: {
+    type: String,
+    enum: ["Todo", "In Progress", "Done"],
+    default: "Todo"
+  },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  status: { type: String, enum: ["Todo", "In Progress", "Done"], default: "Todo" },
-  priority: { type: String, enum: ["Low", "Medium", "High"], default: "Medium" },
+  updatedAt: { type: Date, default: Date.now }
 }, { timestamps: true });
 
-module.exports = mongoose.model("Task", taskSchema);
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
