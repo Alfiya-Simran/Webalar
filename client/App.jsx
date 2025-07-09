95fdbef79fa7e32a6a9acfa70280d8e64a6fb673
Alfiya Simran <alfiyasimran05@gmail.com>
2025-07-09 13:44:05 +0530

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,23 +1,30 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Board from "./pages/Board";
+import { AuthProvider } from "./content/AuthContext";
+import { SocketProvider } from "./content/SocketContext";
 import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route
-  path="/board"
-  element={
-    <ProtectedRoute>
-      <Board />
-    </ProtectedRoute>
-  }
-/>
-    </Routes>
+    <AuthProvider>
+      <SocketProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/board"
+            element={
+              <ProtectedRoute>
+                <Board />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </SocketProvider>
+    </AuthProvider>
   );
 }
 
